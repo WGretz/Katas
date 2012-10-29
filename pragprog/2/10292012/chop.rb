d72d3169a055164d2466e683b1e5db53d7e218eb
Will Gretz <Will@WillGretz.com>
2012-10-29 10:08:59 -0700

    pragprog karate chop kata

---
+++ pragprog/2/10292012/chop.rb
@@ -0,0 +1,42 @@
+require "test/unit"
+
+def chop(item, array)
+  return -1 if array.empty?
+  maximum = array.length - 1
+  minimum = 0
+  while maximum >= minimum
+    midpoint = ((maximum + minimum)/2.0).ceil
+    if item < array[midpoint]
+      maximum = midpoint - 1
+    elsif item > array[midpoint]
+      minimum = midpoint + 1
+    else
+      return midpoint
+    end
+  end
+  return -1
+end
+
+class TestChopIterative < Test::Unit::TestCase
+  def test_chop
+    assert_equal(-1, chop(3, []))
+    assert_equal(-1, chop(3, [1]))
+    assert_equal(0,  chop(1, [1]))
+    assert_equal(0,  chop(1, [1, 3, 5]))
+    assert_equal(1,  chop(3, [1, 3, 5]))
+    assert_equal(2,  chop(5, [1, 3, 5]))
+    assert_equal(-1, chop(0, [1, 3, 5]))
+    assert_equal(-1, chop(2, [1, 3, 5]))
+    assert_equal(-1, chop(4, [1, 3, 5]))
+    assert_equal(-1, chop(6, [1, 3, 5]))
+    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  end
+end
